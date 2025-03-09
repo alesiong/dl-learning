611b64c5899ddaf524794c94505136c3e83660ea
alesiong <ypf7.10@hotmail.com>
2025-03-09 18:24:02 +0800

    add rnn

---
+++ rnn-gen.py
@@ -0,0 +1,225 @@
+import glob
+import os
+import random
+import string
+import time
+from io import open
+
+import math
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import unicodedata
+
+# Check if CUDA is available
+device = torch.device('cpu')
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+# mps is very slow for RNN, don't use it
+# elif torch.backends.mps.is_available():
+#     device = torch.device('mps')
+
+
+all_letters = string.ascii_letters + " .,;'-"
+n_letters = len(all_letters) + 1  # Plus EOS marker
+
+
+def find_files(path): return glob.glob(path)
+
+
+# Turn a Unicode string to plain ASCII, thanks to https://stackoverflow.com/a/518232/2809427
+def unicode_to_ascii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+        and c in all_letters
+    )
+
+
+# Read a file and split into lines
+def read_lines(filename):
+    with open(filename, encoding='utf-8') as some_file:
+        return [unicode_to_ascii(line.strip()) for line in some_file]
+
+
+# Build the category_lines dictionary, a list of lines per category
+category_lines = {}
+all_categories = []
+for filename in find_files('data/RNN/names/*.txt'):
+    category = os.path.splitext(os.path.basename(filename))[0]
+    all_categories.append(category)
+    lines = read_lines(filename)
+    category_lines[category] = lines
+
+n_categories = len(all_categories)
+
+if n_categories == 0:
+    raise RuntimeError('Data not found. Make sure that you downloaded data '
+                       'from https://download.pytorch.org/tutorial/data.zip and extract it to '
+                       'the current directory.')
+
+print('# categories:', n_categories, all_categories)
+
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.i2h = nn.Linear(n_categories + input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(n_categories + input_size + hidden_size, output_size)
+        self.o2o = nn.Linear(hidden_size + output_size, output_size)
+        self.dropout = nn.Dropout(0.1)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, category, input, hidden):
+        input_combined = torch.cat((category, input, hidden), 1)
+        hidden = self.i2h(input_combined)
+        output = self.i2o(input_combined)
+        output_combined = torch.cat((hidden, output), 1)
+        output = self.o2o(output_combined)
+        output = self.dropout(output)
+        output = self.softmax(output)
+        return output, hidden
+
+    def init_hidden(self):
+        return torch.zeros(1, self.hidden_size, device=device)
+
+
+# Get a random category and random line from that category
+def random_training_pair():
+    category = random.choice(all_categories)
+    line = random.choice(category_lines[category])
+    return category, line
+
+
+# One-hot vector for category
+def category_tensor(category):
+    li = all_categories.index(category)
+    tensor = torch.zeros(1, n_categories, device=device)
+    tensor[0][li] = 1
+    return tensor
+
+
+# One-hot matrix of first to last letters (not including EOS) for input
+def input_tensor(line):
+    tensor = torch.zeros(len(line), 1, n_letters, device=device)
+    for li in range(len(line)):
+        letter = line[li]
+        tensor[li][0][all_letters.find(letter)] = 1
+    return tensor
+
+
+# ``LongTensor`` of second letter to end (EOS) for target
+def target_tensor(line):
+    letter_indexes = [all_letters.find(line[li]) for li in range(1, len(line))]
+    letter_indexes.append(n_letters - 1)  # EOS
+    return torch.tensor(letter_indexes, dtype=torch.long, device=device)
+
+
+# Make category, input, and target tensors from a random category, line pair
+def random_training_example():
+    category, line = random_training_pair()
+    category_tensor_ = category_tensor(category)
+    input_line_tensor = input_tensor(line)
+    target_line_tensor = target_tensor(line)
+    return category_tensor_, input_line_tensor, target_line_tensor
+
+
+criterion = nn.NLLLoss().to(device)
+
+learning_rate = 0.0005
+
+rnn = RNN(n_letters, 128, n_letters).to(device)
+
+
+def train(category_tensor, input_line_tensor, target_line_tensor):
+    target_line_tensor.unsqueeze_(-1)
+    hidden = rnn.init_hidden()
+
+    rnn.zero_grad()
+
+    loss = torch.tensor([0], dtype=torch.float32, device=device)  # you can also just simply use ``loss = 0``
+
+    for i in range(input_line_tensor.size(0)):
+        output, hidden = rnn(category_tensor, input_line_tensor[i], hidden)
+        l = criterion(output, target_line_tensor[i])
+        loss += l
+
+    loss.backward()
+
+    for p in rnn.parameters():
+        p.data.add_(p.grad.data, alpha=-learning_rate)
+
+    return output, loss.item() / input_line_tensor.size(0)
+
+
+def time_since(since):
+    now = time.time()
+    s = now - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+
+n_iters = 100000
+print_every = 5000
+plot_every = 500
+all_losses = []
+total_loss = 0  # Reset every ``plot_every`` ``iters``
+
+start = time.time()
+
+for iter in range(1, n_iters + 1):
+    output, loss = train(*random_training_example())
+    total_loss += loss
+
+    if iter % print_every == 0:
+        print('%s (%d %d%%) %.4f' % (time_since(start), iter, iter / n_iters * 100, loss))
+
+    if iter % plot_every == 0:
+        all_losses.append(total_loss / plot_every)
+        total_loss = 0
+
+
+plt.figure()
+plt.plot(all_losses)
+plt.show()
+
+max_length = 20
+
+# Sample from a category and starting letter
+def sample(category, start_letter='A'):
+    with torch.no_grad():  # no need to track history in sampling
+        category_tensor_ = category_tensor(category)
+        input = input_tensor(start_letter)
+        hidden = rnn.init_hidden()
+
+        output_name = start_letter
+
+        for i in range(max_length):
+            output, hidden = rnn(category_tensor_, input[0], hidden)
+            topv, topi = output.topk(1)
+            topi = topi[0][0]
+            if topi == n_letters - 1:
+                break
+            else:
+                letter = all_letters[topi]
+                output_name += letter
+            input = input_tensor(letter)
+
+        return output_name
+
+# Get multiple samples from one category and multiple starting letters
+def samples(category, start_letters='ABC'):
+    for start_letter in start_letters:
+        print(sample(category, start_letter))
+
+samples('Russian', 'RUS')
+
+samples('German', 'GER')
+
+samples('Spanish', 'SPA')
+
+samples('Chinese', 'CHI')
+
